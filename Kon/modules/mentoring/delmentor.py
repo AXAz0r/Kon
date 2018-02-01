504841c57b6c1d336719ce181db6c08328b0e79c
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-01-31 21:42:15 -0500

    corrected errors

--- Kon/modules/mentoring/delmentor.py
+++ Kon/modules/mentoring/delmentor.py
@@ -2,23 +2,25 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    target = discord.utils.find(lambda x: x.name.lower() == 'head mentor', message.guild.roles)
     try:
-        if target.id in [y.id for y in message.author.roles]:
-            fn = 'lists/mentors.txt'
-            target = message.mentions[0]
+        role = discord.utils.find(lambda x: x.name.lower() == 'head mentor', message.guild.roles)
+        if role.id in [y.id for y in message.author.roles]:
+            if message.mentions:
+                target = "%s" % message.mentions[0].id
+            else:
+                msg = ' '.join(args)
+                target = msg.lower()
+            fn = 'lists/requests.txt'
             a = open(fn)
             output = []
             for line in a:
-                if target.id not in line:
+                if target not in line:
                     output.append(line)
             a.close()
             a = open(fn, 'w')
             a.writelines(output)
             a.close()
-            mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
-            await target.remove_roles(mentor_role)
-            response = discord.Embed(title=f"✅ {target.display_name} is no longer a Mentor", color=0xA5FFF6)
+            response = discord.Embed(title="📝 Updated", color=0xA5FFF6)
         else:
             response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
     except AttributeError:
