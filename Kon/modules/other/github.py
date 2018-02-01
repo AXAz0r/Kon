61c84474658600ee110fab7b54584c7b75a2dfeb
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-01 02:03:22 -0500

    replaced with info

--- Kon/modules/other/github.py
+++
@@ -1,7 +0,0 @@
-import discord
-
-
-async def ex(args, message, bot, invoke):
-    embed = discord.Embed(title=':information_source: GitHub for Kon:', color=0xA5FFF6)
-    embed.description = 'https://github.com/Shifty6/Kon'
-    await message.channel.send(embed=embed)
