a6b91bcc31fb2565dbfe9c138418ece75ac68dbe
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-01 22:10:04 -0500

    updated latency

--- Kon/modules/other/ping.py
+++ Kon/modules/other/ping.py
@@ -2,6 +2,5 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    bot_latency = round(bot.latency * 1000, 2)
-    response = discord.Embed(description=f"`🏓 {bot_latency}ms`", color=0xA5FFF6)
+    response = discord.Embed(description=f"`🏓 {int(bot.latency * 1000)}ms`", color=0xA5FFF6)
     await message.channel.send(embed=response)
