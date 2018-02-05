fb4ec93978239f6bd5148f098e472063a99cc803
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-05 01:37:46 -0500

    rewrote command; allowed for no guess

--- Kon/modules/other/roll.py
+++ Kon/modules/other/roll.py
@@ -3,22 +3,30 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    try:
-        guess = abs(int(args[0]))
-        if guess < 7:
-            if guess > 0:
-                die_value = secrets.randbelow(5) + 1
-                if die_value == guess:
-                    sym = '✔'
+    die_value = secrets.randbelow(5) + 1
+    if args:
+        try:
+            guess = abs(int(args[0]))
+            if guess < 7:
+                if guess > 0:
+                    if die_value == guess:
+                        sym = '✔'
+                    else:
+                        sym = '❌'
+                    tmp = f'\n**Your Guess** `{guess}`\n**Match:** `{sym}`'
+                    desc = f'🎲 **Dice**\n**You Rolled** `{die_value}`{tmp}'
+                    color = 0xA5FFF6
                 else:
-                    sym = '❌'
-                response = discord.Embed(color=0xA5FFF6)
-                response.add_field(name='🎲 Dice',
-                                   value=f'**You Rolled** `{die_value}`\n**Your Guess** `{guess}`\n**Match:** `{sym}`')
+                    desc = '❗ Guess must be positive and not a zero'
+                    color = 0xBE1931
             else:
-                response = discord.Embed(description='❗ Guess must be positive and not a zero', color=0xBE1931)
-        else:
-            response = discord.Embed(description='❗ Guess cannot be greater than six', color=0xBE1931)
-    except ValueError:
-        response = discord.Embed(description='❗ Guess must be a number', color=0xBE1931)
+                desc = '❗ Guess cannot be greater than six'
+                color = 0xBE1931
+        except ValueError:
+            desc = '❗ Guess must be a number'
+            color = 0xBE1931
+    else:
+        desc = f'🎲 **Dice**\n**You Rolled** `{die_value}`'
+        color = 0xA5FFF6
+    response = discord.Embed(description=f'{desc}', color=color)
     await message.channel.send(embed=response)
