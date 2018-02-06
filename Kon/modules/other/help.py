67e6a9ed0c8503b04b9ae4712efac6a14750ba28
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-05 22:45:02 -0500

    changed apply line

--- Kon/modules/other/help.py
+++ Kon/modules/other/help.py
@@ -3,7 +3,7 @@ import discord
 
 async def ex(args, message, bot, invoke):
     embed = discord.Embed(title="❔ Hi! I can add your name to the Student Requests list!", color=0xA5FFF6)
-    embed.description = 'Type `^apply @username YourTimeZone` to be added to the list.\n' \
+    embed.description = 'Type `^apply @username UTC #` to be added to the list.\n' \
                         'Type `^requests` to get the current Student Requests list.\n' \
                         'Type `^mentors` to get a list of the current Mentors.\n' \
                         'Type `^modules` to get a list of my modules and their commands.\n' \
