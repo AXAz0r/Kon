28e1865a2ec6a63181d1b3b5c72ddbc4720b1f9a
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-05 22:43:26 -0500

    rewrote to add formatting

--- Kon/modules/mentoring/addmentor.py
+++ Kon/modules/mentoring/addmentor.py
@@ -5,14 +5,20 @@ async def ex(args, message, bot, invoke):
     try:
         target = discord.utils.find(lambda x: x.name.lower() == 'head mentor', message.guild.roles)
         if target.id in [y.id for y in message.author.roles]:
-            target = message.mentions[0]
-            msg = ' '.join(args)
-            a = open('lists/mentors.txt', 'a')
-            a.write('%s' % msg + '\n')
-            a.close()
-            mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
-            await target.add_roles(mentor_role)
-            response = discord.Embed(title=f"✅ {target.display_name} is now a Mentor", color=0xA5FFF6)
+            if len(args) >= 3:
+                user = args[0]
+                zone = args[1]
+                time = args[2]
+                target = message.mentions[0]
+                a = open('lists/mentors.txt', 'a')
+                a.write('`%s %s`: %s' % (zone, time, user) + '\n')
+                a.close()
+                mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
+                await target.add_roles(mentor_role)
+                response = discord.Embed(title=f"✅ {target.display_name} is now a Mentor", color=0xA5FFF6)
+            else:
+                response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
+                response.set_footer(text='@Username UTC and the appropriate number')
         else:
             response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
     except AttributeError:
