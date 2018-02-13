89ae1fb197107b9da14411812882959eaa45a085
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-13 18:48:50 -0500

    added line not found reply

--- Kon/modules/moderative/unraidban.py
+++ Kon/modules/moderative/unraidban.py
@@ -3,24 +3,27 @@ import discord
 
 async def ex(args, message, bot, invoke):
     if message.author.permissions_in(message.channel).administrator:
-        allow = True
-    else:
-        allow = False
-    if not allow:
-        response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
-    else:
         fn = 'lists/raidbans.txt'
+        targetid = "%s" % message.mentions[0].id
         target = message.mentions[0]
         a = open(fn)
         output = []
+        temp = 0
         for line in a:
-            if target.id not in line:
+            if targetid not in line:
                 output.append(line)
+            else:
+                temp += 1
         a.close()
         a = open(fn, 'w')
         a.writelines(output)
         a.close()
         raidban_role = discord.utils.get(message.guild.roles, name='Raid Banned')
         await target.remove_roles(raidban_role)
-        response = discord.Embed(title=f"🔓 {target.display_name} is no longer Raid Banned", color=0xFFCC4d)
+        if not temp == 0:
+            response = discord.Embed(title=f"🔓 {target.display_name} is no longer Raid Banned", color=0xFFCC4d)
+        else:
+            response = discord.Embed(title="❗ That user wasn\'t Raid Banned", color=0xBE1931)
+    else:
+        response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
     await message.channel.send(embed=response)
