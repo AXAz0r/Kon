8f89ed856f7e877e20f3f35fe7655d7c91a62a7c
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-13 18:49:36 -0500

    relocated admin check

--- Kon/modules/moderative/raidban.py
+++ Kon/modules/moderative/raidban.py
@@ -3,14 +3,6 @@ import discord
 
 async def ex(args, message, bot, invoke):
     if message.author.permissions_in(message.channel).administrator:
-        allow = True
-    elif message.author.id == '208974392644861952':
-        allow = True
-    else:
-        allow = False
-    if not allow:
-        response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
-    else:
         msg = ' '.join(args)
         a = open('lists/raidbans.txt', 'a')
         a.write('%s' % msg + '\n')
@@ -19,4 +11,6 @@ async def ex(args, message, bot, invoke):
         raidban_role = discord.utils.get(message.guild.roles, name='Raid Banned')
         await target.add_roles(raidban_role)
         response = discord.Embed(title=f"🔒 {target.display_name} is now Raid Banned", color=0xFFCC4d)
+    else:        
+        response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
     await message.channel.send(embed=response)
