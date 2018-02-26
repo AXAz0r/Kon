141b48ce8e50f4387c8cfe19d2a4be54d153e05e
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-26 17:33:38 -0500

    created members command

---
+++ Kon/modules/other/members.py
@@ -0,0 +1,30 @@
+import discord
+from checks import private_check
+
+
+async def ex(args, message, bot, invoke):
+    if not private_check(message):
+        gld = message.guild
+        offline_users = len([x for x in message.guild.members if x.status == discord.Status.offline])
+        dnd_users = len([x for x in message.guild.members if x.status == discord.Status.dnd])
+        idle_users = len([x for x in message.guild.members if x.status == discord.Status.idle])
+        online_users = len([x for x in message.guild.members if x.status == discord.Status.online])
+        not_offline = len([x for x in message.guild.members if x.status != discord.Status.offline])
+        user_count = 0
+        for user in gld.members:
+            if not user.bot:
+                user_count += 1
+        active_users = (not_offline / user_count) * 100
+        active_round = round(active_users, 2)
+        other_status = dnd_users + idle_users
+        members = f'\nMembers: **{user_count}**'
+        members += f'\nOffline: **{offline_users}**'
+        members += f'\nOnline: **{online_users}**'
+        members += f'\nOther: **{other_status}**'
+        members += f'\nActive: **{active_round}%**'
+        response = discord.Embed(color=0xA5FFF6)
+        response.add_field(name='👥 Member Status', value=members)
+        response.set_footer(text='Active is users who are not offline')
+    else:
+        response = discord.Embed(title='❗ This command is only available in servers', color=0xBE1931)
+    await message.channel.send(embed=response)
