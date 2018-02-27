5de3b76e43853bd816b858e27868fe594bd647fb
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-27 16:29:57 -0500

    changed write format

--- modules/mentoring/apply.py
+++ modules/mentoring/apply.py
@@ -8,17 +8,17 @@ async def ex(args, message, bot, invoke):
                 zone = args[1]
                 time = args[2]
                 with open('lists/requests.txt', 'a') as a:
-                    a.write('`%s %s`: %s' % (zone, time, user) + '\n')
+                    a.write(f'`{zone} {time}`: {user}' + '\n')
                 response = discord.Embed(title="✅ I put you on the 'Student Requests' list", color=0xA5FFF6)
             elif len(args) >= 2:
                 user = args[0]
                 zone = args[1]
                 with open('lists/requests.txt', 'a') as a:
-                    a.write('`%s`: %s' % (zone, user) + '\n')
+                    a.write(f'`{zone}`: {user}' + '\n')
                 response = discord.Embed(title="✅ I put you on the 'Student Requests' list", color=0xA5FFF6)
             else:
                 response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
-                response.set_footer(text='@Username UTC and the appropriate number')
+                response.set_footer(text='@Username UTC #')
         else:
             response = discord.Embed(title="❗ No input", color=0xBE1931)
         await message.channel.send(embed=response)
