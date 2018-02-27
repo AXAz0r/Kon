8f4ff1931b42d53bb340f94bf87bcc06dd103314
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-02-27 16:29:42 -0500

    changed write format

--- modules/mentoring/addmentor.py
+++ modules/mentoring/addmentor.py
@@ -11,16 +11,16 @@ async def ex(args, message, bot, invoke):
                 time = args[2]
                 target = message.mentions[0]
                 a = open('lists/mentors.txt', 'a')
-                a.write('`%s %s`: %s' % (zone, time, user) + '\n')
+                a.write(f'`{zone} {time}`: {user}' + '\n')
                 a.close()
                 mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
                 await target.add_roles(mentor_role)
                 response = discord.Embed(title=f"✅ {target.display_name} is now a Mentor", color=0xA5FFF6)
             else:
                 response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
-                response.set_footer(text='@Username UTC and the appropriate number')
+                response.set_footer(text='@Username UTC #')
         else:
             response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
     except AttributeError:
-        response = discord.Embed(title="❗ I couldn\'t find the role 'Head Mentor'")
+        response = discord.Embed(title="❗ I couldn\'t find the role 'Head Mentor'", color=0xBE1931)
     await message.channel.send(embed=response)
