db4562dd2dc8dc527b58d4ae415b3d40572c5889
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-01 00:52:52 -0500

    added hyperlink command

---
+++ modules/other/hyperlink.py
@@ -0,0 +1,8 @@
+import discord
+
+
+async def ex(args, message, bot, invoke):
+    link_name = ' '.join(args[1:])
+    url_string = args[0].lower()
+    response = discord.Embed(description=f'[{link_name}]({url_string})')
+    await message.channel.send(embed=response)
