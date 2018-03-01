9ad72b0e1f342e21ac80920e694b66e3d67cf36e
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-01 15:45:17 -0500

    changed input format and added checks

--- modules/other/hyperlink.py
+++ modules/other/hyperlink.py
@@ -3,10 +3,20 @@ import discord
 
 async def ex(args, message, bot, invoke):
     await message.delete()
-    link_name = ' '.join(args[1:])
-    url_string = args[0].lower()
-    if "http" in url_string:
-        response = discord.Embed(description=f'🌐 [{link_name}]({url_string})', color=0x5DBCD2)
+    if args:
+        all_qry = ' '.join(args)
+        if all_qry.endswith(';'):
+            all_qry = all_qry[:-1]
+        url_string = all_qry.split('; ')[-1]
+        link_name = '; '.join(all_qry.split('; ')[:-1])
+        if link_name is not '':
+            if "http" in url_string:
+                response = discord.Embed(description=f'🌐 [{link_name}]({url_string})', color=0x5DBCD2)
+            else:
+                response = discord.Embed(title='❗ URL must include http/s', color=0xBE1931)
+        else:
+            response = discord.Embed(title='❗ Separate the hyperlink text from the URL with ; then a space',
+                                     color=0xBE1931)
     else:
-        response = discord.Embed(title='❗ URL must include http/s', color=0xBE1931)
+        response = discord.Embed(title='❗ No input', color=0xBE1931)
     await message.channel.send(embed=response)
