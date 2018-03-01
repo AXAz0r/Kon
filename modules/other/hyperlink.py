cdd10e6f705f6015a6b2cdd4d9d17d875cae252b
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-01 01:01:14 -0500

    added delete message line

--- modules/other/hyperlink.py
+++ modules/other/hyperlink.py
@@ -2,6 +2,7 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
+    await message.delete()
     link_name = ' '.join(args[1:])
     url_string = args[0].lower()
     response = discord.Embed(description=f'[{link_name}]({url_string})')
