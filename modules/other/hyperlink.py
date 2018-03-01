74c9ea212b38467250182786f3be3826f4aa54df
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-01 01:09:33 -0500

    added embed color and icon

--- modules/other/hyperlink.py
+++ modules/other/hyperlink.py
@@ -5,5 +5,5 @@ async def ex(args, message, bot, invoke):
     await message.delete()
     link_name = ' '.join(args[1:])
     url_string = args[0].lower()
-    response = discord.Embed(description=f'[{link_name}]({url_string})')
+    response = discord.Embed(description=f'🌐 [{link_name}]({url_string})', color=0x5DBCD2)
     await message.channel.send(embed=response)
