31d4bf9d8e807d82f29f6ef5adf3aa3308372952
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-01 01:29:03 -0500

    added http check

--- modules/other/hyperlink.py
+++ modules/other/hyperlink.py
@@ -5,5 +5,8 @@ async def ex(args, message, bot, invoke):
     await message.delete()
     link_name = ' '.join(args[1:])
     url_string = args[0].lower()
-    response = discord.Embed(description=f'🌐 [{link_name}]({url_string})', color=0x5DBCD2)
+    if "http" in url_string:
+        response = discord.Embed(description=f'🌐 [{link_name}]({url_string})', color=0x5DBCD2)
+    else:
+        response = discord.Embed(title='❗ URL must include http/s', color=0xBE1931)
     await message.channel.send(embed=response)
