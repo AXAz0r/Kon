138a950498af5deba7c0dc5247a82602ef5da8ce
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-01 19:50:53 -0500

    removed file do to being obsolete

--- modules/moderative/raidbans.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-
-async def ex(args, message, bot, invoke):
-    with open('lists/raidbans.txt', 'r') as file:
-        a = file.read()
-    embed = discord.Embed(title="**Raid Bans:**", color=0xA5FFF6)
-    embed.description = ('%s' % a)
-    await message.channel.send(embed=embed)
