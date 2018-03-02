2ddb8349beec88845627a533fc910129b4fabe1d
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-02 14:49:37 -0500

    added url check

--- modules/other/link.py
+++ modules/other/link.py
@@ -7,13 +7,16 @@ async def ex(args, message, bot, invoke):
         all_qry = ' '.join(args)
         if all_qry.endswith(';'):
             all_qry = all_qry[:-1]
-        url_string = all_qry.split('; ')[-1]
-        link_name = '; '.join(all_qry.split('; ')[:-1])
+        link_name = ''.join(all_qry.split('; ')[0])
+        url_string = all_qry.split('; ')[1]
         if link_name is not '':
             if "http" in url_string:
-                avatar = 'https://i.imgur.com/tUo5WrQ.png'
-                response = discord.Embed(description=f'[{link_name}]({url_string})', color=0x0f80a6)
-                response.set_author(name='Hyperlink', icon_url=avatar)
+                if ' ' not in url_string:
+                    avatar = 'https://i.imgur.com/tUo5WrQ.png'
+                    response = discord.Embed(description=f'[{link_name}]({url_string})\n', color=0x0f80a6)
+                    response.set_author(name='Hyperlink', icon_url=avatar)
+                else:
+                    response = discord.Embed(title='❗ URL cannot contain spaces', color=0xBE1931)
             else:
                 response = discord.Embed(title='❗ URL must include http/s', color=0xBE1931)
         else:
