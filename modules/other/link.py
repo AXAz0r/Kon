bb6639ae688ee5f4fee4366f8c167f8b6f34dfc4
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-02 22:20:39 -0500

    allowed for multiple links and added errors

--- modules/other/link.py
+++ modules/other/link.py
@@ -3,25 +3,41 @@ import discord
 
 async def ex(args, message, bot, invoke):
     await message.delete()
-    if args:
-        all_qry = ' '.join(args)
-        if all_qry.endswith(';'):
-            all_qry = all_qry[:-1]
-        link_name = ''.join(all_qry.split('; ')[0])
-        url_string = all_qry.split('; ')[1]
-        if link_name is not '':
-            if "http" in url_string:
-                if ' ' not in url_string:
-                    avatar = 'https://i.imgur.com/tUo5WrQ.png'
-                    response = discord.Embed(description=f'[{link_name}]({url_string})\n', color=0x0f80a6)
-                    response.set_author(name='Hyperlink', icon_url=avatar)
-                else:
-                    response = discord.Embed(title='❗ URL cannot contain spaces', color=0xBE1931)
-            else:
-                response = discord.Embed(title='❗ URL must include http/s', color=0xBE1931)
-        else:
-            response = discord.Embed(title='❗ Separate the hyperlink text from the URL with ; then a space',
-                                     color=0xBE1931)
-    else:
+    if not args:
         response = discord.Embed(title='❗ No input', color=0xBE1931)
-    await message.channel.send(embed=response)
+        await message.channel.send(embed=response)
+        return
+    if not len(args) >= 2:
+        response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
+        await message.channel.send(embed=response)
+        return
+    else:
+        err = ''
+        desc = ''
+        link_num = 0
+        all_qry = ' '.join(args)
+        hyp_qrys = ' > '.join(all_qry.split(' > ')[0:])
+        hyp_qry = hyp_qrys.split(' > ')
+        for qry in hyp_qry:
+            link_num += 1
+            curr_link = link_num
+            link_name = ''.join(qry.split('; ')[0])
+            url_string = qry.split('; ')[1]
+            if link_name is not '':
+                if "http" in url_string:
+                    if ' ' not in url_string:
+                        desc += f'[{link_name}]({url_string})\n'
+                    else:
+                        err += f'**Link #{curr_link}** `URL cannot contain spaces`\n'
+                else:
+                    err += f'**Link #{curr_link}** `URL must include http/s`\n'
+
+            else:
+                err += f'**Link #{curr_link}** `Format: Google Home; https://www.google.com`\n'
+        if err is not '':
+            err_header = f'**Error:**'
+        else:
+            err_header = ''
+        response = discord.Embed(description=f'{desc} \n{err_header}\n{err}', color=0x0f80a6)
+        response.set_author(name='Hyperlink', icon_url='https://i.imgur.com/tUo5WrQ.png')
+        await message.channel.send(embed=response)
