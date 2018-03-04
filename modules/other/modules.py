3952eca0a844fc969fa90ef89dbad8479ba03c51
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-03 23:24:03 -0500

    changed moderative to owner

--- modules/other/modules.py
+++ modules/other/modules.py
@@ -5,8 +5,8 @@ async def ex(args, message, bot, invoke):
     response = discord.Embed(description='**There are 4 modules**\n'
                                       '```yml\n'
                                       '- MENTORING\n'
-                                      '- MODERATIVE\n'
                                       '- OTHER\n'
+                                      '- OWNER\n'
                                       '- VOTING```', color=0xA5FFF6)
     response.set_footer(text=f'^commands module_name')
     await message.channel.send(embed=response)
