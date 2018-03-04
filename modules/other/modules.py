fdf58a31dd839d0f7cbed7b2ca8408b4506864c0
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-03 19:43:17 -0500

    changed embed variable name

--- modules/other/modules.py
+++ modules/other/modules.py
@@ -2,11 +2,11 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    embed = discord.Embed(description='**There are 4 modules**\n'
+    response = discord.Embed(description='**There are 4 modules**\n'
                                       '```yml\n'
                                       '- MENTORING\n'
                                       '- MODERATIVE\n'
                                       '- OTHER\n'
                                       '- VOTING```', color=0xA5FFF6)
-    embed.set_footer(text=f'^commands module_name')
-    await message.channel.send(embed=embed)
+    response.set_footer(text=f'^commands module_name')
+    await message.channel.send(embed=response)
