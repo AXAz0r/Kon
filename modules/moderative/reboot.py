661b85feaee6116a0c6addbf2209c2cea5331afa
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-03 19:41:55 -0500

    changed embed color

--- modules/moderative/reboot.py
+++ modules/moderative/reboot.py
@@ -4,7 +4,7 @@ from checks import owner_check
 
 async def ex(args, message, bot, invoke):
     if owner_check(message):
-        await message.channel.send(embed=discord.Embed(title="⚙ Rebooting", color=0xA5FFF6))
+        await message.channel.send(embed=discord.Embed(title="⚙ Rebooting", color=0x66757F))
         await bot.logout()
     else:
         await message.channel.send(embed=discord.Embed(title="⛔ You are not the owner", color=0xBE1931))
