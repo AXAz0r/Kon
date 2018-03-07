7150e55e9c8dfd2253a938d247c1aa88e48f3d51
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-07 15:01:19 -0500

    condensed counts and added bots count

--- modules/other/members.py
+++ modules/other/members.py
@@ -5,32 +5,30 @@ from checks import private_check
 async def ex(args, message, bot, invoke):
     if not private_check(message):
         gld = message.guild
-        user_count = 0
-        online_count = 0
-        dnd_count = 0
-        idle_count = 0
-        offline_count = 0
-        active_count = 0
+        counts = [0, 0, 0, 0, 0, 0, 0]
         for user in gld.members:
             if not user.bot:
-                user_count += 1
+                counts[0] += 1
                 if user.status == discord.Status.online:
-                    online_count += 1
+                    counts[1] += 1
                 if user.status == discord.Status.dnd:
-                    dnd_count += 1
+                    counts[2] += 1
                 if user.status == discord.Status.idle:
-                    idle_count += 1
+                    counts[3] += 1
                 if user.status == discord.Status.offline:
-                    offline_count += 1
+                    counts[4] += 1
                 if user.status != discord.Status.offline:
-                    active_count += 1
-        active_users = (active_count / user_count) * 100
+                    counts[5] += 1
+            else:
+                counts[6] += 1
+        active_users = (counts[5] / counts[0]) * 100
         active_round = round(active_users, 2)
-        other_count = dnd_count + idle_count
-        members = f'\nMembers: **{user_count}**'
-        members += f'\nOnline: **{online_count}**'
-        members += f'\nOffline: **{offline_count}**'
+        other_count = counts[2] + counts[3]
+        members = f'\nMembers: **{counts[0]}**'
+        members += f'\nOnline: **{counts[1]}**'
+        members += f'\nOffline: **{counts[4]}**'
         members += f'\nOther: **{other_count}**'
+        members += f'\nBots: **{counts[6]}**'
         members += f'\nActive: **{active_round}%**'
         response = discord.Embed(color=0x66757F)
         response.add_field(name='👥 Member Status', value=members)
