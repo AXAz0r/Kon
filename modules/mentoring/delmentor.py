1c044979cfceaca0e4fb8ee83b1576357be42008
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-08 15:43:26 -0500

    changed target

--- modules/mentoring/delmentor.py
+++ modules/mentoring/delmentor.py
@@ -6,13 +6,12 @@ async def ex(args, message, bot, invoke):
         target = discord.utils.find(lambda x: x.name.lower() == 'head mentor', message.guild.roles)
         if target.id in [y.id for y in message.author.roles]:
             fn = 'lists/mentors.txt'
-            targetid = "%s" % message.mentions[0].id
             target = message.mentions[0]
             a = open(fn)
             output = []
             temp = 0
             for line in a:
-                if targetid not in line:
+                if f'{target.id}' not in line:
                     output.append(line)
                 else:
                     temp += 1
@@ -23,11 +22,11 @@ async def ex(args, message, bot, invoke):
             mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
             await target.remove_roles(mentor_role)
             if not temp == 0:
-                response = discord.Embed(title=f"✅ {target.display_name} is no longer a Mentor", color=0x77B255)
+                response = discord.Embed(title=f"✅ {target.display_name} is no longer a Mentor", color=0x77b255)
             else:
-                response = discord.Embed(title="❗ That user wasn\'t a Mentor", color=0xBE1931)
+                response = discord.Embed(title="❗ That user wasn't a Mentor", color=0xBE1931)
         else:
             response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
     except AttributeError:
-        response = discord.Embed(title="❗ I couldn\'t find the role 'Head Mentor'")
+        response = discord.Embed(title="❗ I couldn't find the role 'Head Mentor'", color=0xBE1931)
     await message.channel.send(embed=response)
