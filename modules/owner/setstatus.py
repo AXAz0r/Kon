165104d8945a89d57bcce94401a6cbe1ae56a064
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-09 15:20:22 -0500

    added owner_check and args check

--- modules/owner/setstatus.py
+++ modules/owner/setstatus.py
@@ -1,9 +1,22 @@
 import discord
+import json
+from checks import owner_check
 
 
 async def ex(args, message, bot, invoke):
-    status = ' '.join(args)
-    game = discord.Game(name=status)
-    await bot.change_presence(game=game)
-    response = discord.Embed(color=0x77B255, title=f'✅ New playing status set to {status}.')
+    if owner_check(message):
+        if args:
+            status = ' '.join(args)
+            activity = discord.Activity(type=discord.ActivityType.playing, name=status)
+            await bot.change_presence(activity=activity)
+            response = discord.Embed(title=f'✅ New playing status set to {status}.', color=0x77B255)
+            with open('config.json', 'r') as config_data:
+                status_data = json.load(config_data)
+            status_data['Status'] = f'{status}'
+            with open('config.json', 'w') as config_data:
+                json.dump(status_data, config_data, sort_keys=True, indent=4)
+        else:
+            response = discord.Embed(itle='❗ Give me a status to change to, please.', color=0xBE1931)
+    else:
+        response = discord.Embed(title="⛔ You are not the owner", color=0xBE1931)
     await message.channel.send(embed=response)
