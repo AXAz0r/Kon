7e36a69c625c4489e31a0ded0e9fcf4d1cdb48b6
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-09 15:20:55 -0500

    added owner_check

--- modules/owner/setavatar.py
+++ modules/owner/setavatar.py
@@ -1,21 +1,25 @@
 import aiohttp
 import discord
+from checks import owner_check
 
 
 async def ex(args, message, bot, invoke):
-    if args or message.attachments:
-        if message.attachments:
-            image_url = message.attachments[0].url
+    if owner_check(message):
+        if args or message.attachments:
+            if message.attachments:
+                image_url = message.attachments[0].url
+            else:
+                image_url = ' '.join(args)
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(image_url) as image_response:
+                        img_data = await image_response.read()
+                await bot.user.edit(avatar=img_data)
+                response = discord.Embed(title=f'✅ My avatar has been changed.', color=0x77B255)
+            except discord.Forbidden:
+                response = discord.Embed(title=f'❗ I was unable to change my avatar.', color=0xBE1931)
         else:
-            image_url = ' '.join(args)
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(image_url) as image_response:
-                    img_data = await image_response.read()
-            await bot.user.edit(avatar=img_data)
-            response = discord.Embed(title=f'✅ My avatar has been changed.', color=0x77B255)
-        except discord.Forbidden:
-            response = discord.Embed(title=f'❗ I was unable to change my avatar.', color=0xBE1931)
+            response = discord.Embed(itle='❗ Give me a link or attach an image, please.', color=0xBE1931)
     else:
-        response = discord.Embed(itle='❗ Give me a link or attach an image, please.', color=0xBE1931)
+        response = discord.Embed(title="⛔ You are not the owner", color=0xBE1931)
     await message.channel.send(embed=response)
