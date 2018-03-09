51b8a36927b256e00b9d78d4acb6852b273db053
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-09 15:19:24 -0500

    added owner_check and args check

--- modules/owner/setusername.py
+++ modules/owner/setusername.py
@@ -1,11 +1,18 @@
 import discord
+from checks import owner_check
 
 
 async def ex(args, message, bot, invoke):
-    name_input = ' '.join(args)
-    try:
-        await bot.user.edit(username=name_input)
-        response = discord.Embed(color=0x77B255, title=f'✅ Changed username to {name_input}.')
-    except discord.Forbidden:
-        response = discord.Embed(color=0xBE1931, title=f'❗ I was unable to change my username.')
+    if owner_check(message):
+        if args:
+            try:
+                name_input = ' '.join(args)
+                await bot.user.edit(username=name_input)
+                response = discord.Embed(color=0x77B255, title=f'✅ Changed username to {name_input}.')
+            except discord.Forbidden:
+                response = discord.Embed(color=0xBE1931, title=f'❗ I was unable to change my username.')
+        else:
+            response = discord.Embed(itle='❗ Give me a username to change to, please.', color=0xBE1931)
+    else:
+        response = discord.Embed(title="⛔ You are not the owner", color=0xBE1931)
     await message.channel.send(embed=response)
