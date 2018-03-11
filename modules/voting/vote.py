3875ea4388093ae0268b8d71f59a207c09706a91
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-11 16:36:37 -0400

    Rewrote auth checking

--- modules/voting/vote.py
+++ modules/voting/vote.py
@@ -2,7 +2,7 @@ import json
 import os
 import discord
 import asyncio
-from checks import private_check, server_check
+from checks import dm_check, server_check
 
 
 def dump_vote(vote_data, vote_key, voter):
@@ -20,109 +20,93 @@ def dump_vote(vote_data, vote_key, voter):
         json.dump(vote_data, vote_data_file)
 
 
-def role_check(message):
-    with open('permissions/roles.txt') as file:
-        a = file.read()
-    roles = [y.id for y in message.author.roles]
-    has_role = False
-    for role in a:
-        if role in roles:
-            has_role = True
+def check_roles(allowed_roles, all_users, user):
+    members = []
+    for member in all_users:
+        if isinstance(member, discord.Member):
+            if member.id == user.id:
+                members.append(member)
+    authorized = False
+    for member_item in members:
+        for allowed_role in allowed_roles:
+            role = discord.utils.find(lambda x: x.id == allowed_role, member_item.roles)
+            if role:
+                authorized = True
+                break
+        if authorized:
             break
-    return has_role
+    return authorized
 
 
-async def ex(args, message, bot, invoke):
-    if private_check(message):
-        with open('permissions/registered.txt', 'r') as file:
-            a = file.read()
-        with open('permissions/warlords.txt', 'r') as file:
-            b = file.read()
-        with open('permissions/generals.txt', 'r') as file:
-            c = file.read()
-        with open('permissions/officers.txt', 'r') as file:
-            d = file.read()
-        msg = "%s" % message.author.id
-        if msg in a:
-            auth = True
-        elif msg in b:
-            auth = True
-        elif msg in c:
-            auth = True
-        elif msg in d:
+def auth_check(message):
+    if message.guild:
+        if server_check(message):
             auth = True
         else:
             auth = False
-        if auth:
-            if os.path.exists('lists/vote_data.json'):
-                with open('lists/vote_data.json', encoding='utf-8') as vote_data_file:
-                    vote_data = json.loads(vote_data_file.read())
-            else:
-                vote_data = {}
-            voted = vote_data.get('voters') or []
-            if message.author.id not in voted:
-                msg = ' '.join(args).lower()
-                if msg == 'yes':
-                    dump_vote(vote_data, 'yes', message.author)
-                    response = discord.Embed(title='🗳️ Voted!', color=0x9AAAB4)
-                elif msg == 'no':
-                    dump_vote(vote_data, 'no', message.author)
-                    response = discord.Embed(title='🗳️ Voted!', color=0x9AAAB4)
-                else:
-                    response = discord.Embed(title="❗ Vote must be 'yes' or 'no'", color=0xBE1931)
-            else:
-                response = discord.Embed(title='⛔ You already voted', color=0xBE1931)
-        else:
-            response = discord.Embed(title='⛔ You must register on the server to vote in a DM', color=0xBE1931)
-        await message.channel.send(embed=response)
     else:
-        if server_check(message):
-            try:
-                await message.delete()
-            except PermissionError:
-                response = discord.Embed(title='❗ I don\'t have Manage Messages in this channel', color=0xBE1931)
-            with open('permissions/channels.txt', 'r') as file:
-                a = file.read()
-            chn = "%s" % message.channel.id
-            if chn in a:
-                channel = True
-            else:
-                channel = False
-            with open('permissions/users.txt', 'r') as file:
-                a = file.read()
-            msg = "%s" % message.author.id
-            if msg in a:
-                user = True
-            else:
-                user = False
-            if channel:
-                try:
-                    if user or role_check:
-                        if os.path.exists('lists/vote_data.json'):
-                            with open('lists/vote_data.json', encoding='utf-8') as vote_data_file:
-                                vote_data = json.loads(vote_data_file.read())
-                        else:
-                            vote_data = {}
-                        voted = vote_data.get('voters') or []
-                        msg = ' '.join(args).lower()
-                        if message.author.id not in voted:
-                            if msg == 'yes':
-                                dump_vote(vote_data, 'yes', message.author)
-                                response = discord.Embed(title='🗳️ Voted!', color=0x9AAAB4)
-                            elif msg == 'no':
-                                dump_vote(vote_data, 'no', message.author)
-                                response = discord.Embed(title='🗳️ Voted!', color=0x9AAAB4)
-                            else:
-                                response = discord.Embed(title="❗ Vote must be 'yes' or 'no'", color=0xBE1931)
-                        else:
-                            response = discord.Embed(title='⛔ You already voted', color=0xBE1931)
-                except UnboundLocalError:
-                    response = discord.Embed(title='🔒 You do not have the required roles', color=0xFFCC4d)
-            else:
-                response = discord.Embed(title='🔒 You can\'t use that command in this channel', color=0xFFCC4d)
+        if dm_check():
+            auth = True
         else:
-            response = discord.Embed(title='🔒 You can\'t use that command on this server', color=0xFFCC4d)
-        del_response = await message.channel.send(embed=response)
+            auth = False
+    return auth
+
+
+async def ex(args, message, bot, invoke):
+    if message.guild:
+        try:
+            await message.delete()
+        except PermissionError:
+            response = discord.Embed(title='❗ I don\'t have Manage Messages in this channel', color=0xBE1931)
+            await message.channel.send(embed=response)
+    if auth_check(message):
+        with open('permissions/channels.txt', 'r') as file:
+            a = file.read()
+        if f'{message.channel.id}' in a:
+            channel = True
+        else:
+            channel = False
+        with open('permissions/users.txt', 'r') as file:
+            a = file.read()
+        if f'{message.author.id}' in a:
+            user = True
+        else:
+            user = False
+        if channel or dm_check():
+            with open('permissions/roles.json') as roles_data:
+                roles_file = json.loads(roles_data.read())
+            rol_p = roles_file['roles']
+            if user or check_roles(rol_p, bot.get_all_members(), message.author):
+                if os.path.exists('lists/vote_data.json'):
+                    with open('lists/vote_data.json', encoding='utf-8') as vote_data_file:
+                        vote_data = json.loads(vote_data_file.read())
+                else:
+                    vote_data = {}
+                voted = vote_data.get('voters') or []
+                msg = ' '.join(args).lower()
+                if message.author.id not in voted:
+                    if msg == 'yes':
+                        dump_vote(vote_data, 'yes', message.author)
+                        response = discord.Embed(title='🗳️ Voted!', color=0x9aaab4)
+                    elif msg == 'no':
+                        dump_vote(vote_data, 'no', message.author)
+                        response = discord.Embed(title='🗳️ Voted!', color=0x9aaab4)
+                    else:
+                        response = discord.Embed(title="❗ Vote must be 'yes' or 'no'", color=0xBE1931)
+                else:
+                    response = discord.Embed(title='⛔ You already voted', color=0xBE1931)
+            else:
+                response = discord.Embed(title='🔒 You do not have the required roles', color=0xFFCC4d)
+        else:
+            response = discord.Embed(title='🔒 You can\'t use that command in this channel', color=0xFFCC4d)
+    else:
+        if message.guild:
+            location = 'on this server'
+        else:
+            location = 'in a DM'
+        response = discord.Embed(title=f'🔒 You can\'t use that command {location}', color=0xFFCC4d)
+    del_response = await message.channel.send(embed=response)
+    if message.guild:
         await asyncio.sleep(4)
         await del_response.delete()
 
