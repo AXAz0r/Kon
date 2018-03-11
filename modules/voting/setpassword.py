d3b5fff705c08010135a37322a991765bb06ea4a
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-11 16:35:38 -0400

    Removed DM from command

--- modules/voting/setpassword.py
+++ modules/voting/setpassword.py
@@ -1,13 +1,12 @@
 import json
-import os
 import random
-import asyncio
+import arrow
 import discord
 import string
-from checks import server_check, private_check
+from checks import server_check
 
 
-def set_pswd(pswd_data, pswd_str, password: str):
+def set_pswd(pswd_data, password: str):
     pswd_str = pswd_data.get('pswd')
     if pswd_str is None:
         pswd_str = []
@@ -18,45 +17,37 @@ def set_pswd(pswd_data, pswd_str, password: str):
         json.dump(pswd_data, pswd_data_file)
 
 
-def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+def id_gen(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def get_pswd():
-    with open('lists/password_data.json', encoding='utf-8') as pswd_data_file:
-        pswd_data = json.loads(pswd_data_file.read())
-        pswd_str = pswd_data.get('pswd') or []
-    for pswd in pswd_str:
-        return pswd
+def set_time(pass_data, time_auth: str):
+    time_str = pass_data.get('time')
+    if time_str is None:
+        time_str = []
+    del time_str
+    pass_data.update({'time': time_auth})
+    with open('lists/password_data.json', 'w', encoding='utf-8') as pass_file:
+        json.dump(pass_data, pass_file)
 
 
 async def ex(args, message, bot, invoke):
-    if private_check:
+    if message.guild:
         if server_check(message):
-            if message.author.permissions_in(message.channel).administrator:
-                if os.path.exists('lists/password_data.json'):
-                    with open('lists/password_data.json', encoding='utf-8') as pswd_data_file:
-                        pswd_data = json.loads(pswd_data_file.read())
-                else:
-                    pswd_data = {}
-                if message.mentions:
-                    target = message.mentions[0]
-                    pronoun = 'them'
-                else:
-                    target = message.author
-                    pronoun = 'you'
-                set_pswd(pswd_data, 'pswd', id_generator())
-                response = discord.Embed(title=f'🔐 Password reset. It will be DM\'d to {pronoun} in 24h', color=0xFFCC4d)
-                await message.channel.send(embed=response)
-                target_reply = discord.Embed(title=f'🔑 Here is the password `{get_pswd()}`', color=0xc1694f)
-                target_reply.set_footer(text=f'^votes/voters password')
-                await asyncio.sleep(86400)
-                await bot.send_message(target, embed=target_reply)
-                return pswd_data
+            if message.author.permissions_in(message.channel).manage_guild:
+                with open('lists/password_data.json', encoding='utf-8') as pswd_data_file:
+                    pswd_data = json.loads(pswd_data_file.read())
+                set_pswd(pswd_data, id_gen())
+                response = discord.Embed(title=f'🔐 Password reset. It will be available in 24h', color=0xFFCC4d)
+                with open('lists/password_data.json', encoding='utf-8') as pass_file:
+                    pass_data = json.loads(pass_file.read())
+                available_stamp = arrow.utcnow().timestamp + 86400
+                set_time(pass_data, available_stamp)
             else:
-                response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
+                response = discord.Embed(title='⛔ Access denied: Manage Server required', color=0xBE1931)
         else:
             response = discord.Embed(title='🔒 You can\'t use that command on this server', color=0xFFCC4d)
     else:
         response = discord.Embed(title='🔒 You can\'t use that command in a DM', color=0xFFCC4d)
     await message.channel.send(embed=response)
+
