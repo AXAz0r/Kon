c9e62657770835e612ec116a487032e02b349ed6
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-11 16:28:02 -0400

    Added multiple checks

--- modules/mentoring/apply.py
+++ modules/mentoring/apply.py
@@ -2,23 +2,33 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
+    if message.guild:
         if args:
-            if len(args) >= 3:
-                user = args[0]
-                zone = args[1]
-                time = args[2]
-                with open('lists/requests.txt', 'a') as a:
-                    a.write(f'`{zone} {time}`: {user}' + '\n')
-                response = discord.Embed(title="✅ I put you on the 'Student Requests' list", color=0x77B255)
-            elif len(args) >= 2:
-                user = args[0]
-                zone = args[1]
-                with open('lists/requests.txt', 'a') as a:
-                    a.write(f'`{zone}`: {user}' + '\n')
-                response = discord.Embed(title="✅ I put you on the 'Student Requests' list", color=0xA5FFF6)
+            if message.mentions:
+                if message.mentions[0].id == message.author.id:
+                    pronoun = 'you'
+                else:
+                    pronoun = 'them'
+                if len(args) >= 3:
+                    user = args[0]
+                    zone = args[1]
+                    time = args[2]
+                    with open('lists/requests.txt', 'a') as a:
+                        a.write(f'`{zone} {time}`: {user}' + '\n')
+                    response = discord.Embed(title=f"✅ I put {pronoun} on the 'Student Requests' list", color=0x77B255)
+                elif len(args) >= 2:
+                    user = args[0]
+                    zone = args[1]
+                    with open('lists/requests.txt', 'a') as a:
+                        a.write(f'`{zone}`: {user}' + '\n')
+                    response = discord.Embed(title=f"✅ I put {pronoun} on the 'Student Requests' list", color=0x77B255)
+                else:
+                    response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
+                    response.set_footer(text='@Username UTC #')
             else:
-                response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
-                response.set_footer(text='@Username UTC #')
+                response = discord.Embed(title='❗ A user mention required', color=0xBE1931)
         else:
             response = discord.Embed(title="❗ No input", color=0xBE1931)
-        await message.channel.send(embed=response)
+    else:
+        response = discord.Embed(title='🔒 You can\'t use that command in a DM', color=0xFFCC4d)
+    await message.channel.send(embed=response)
