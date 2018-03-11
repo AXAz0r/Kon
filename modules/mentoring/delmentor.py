b810f2d213f9f432e928024d619ebddf7e5accdc
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-11 16:28:36 -0400

    Added multiple checks

--- modules/mentoring/delmentor.py
+++ modules/mentoring/delmentor.py
@@ -2,31 +2,40 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    try:
-        target = discord.utils.find(lambda x: x.name.lower() == 'head mentor', message.guild.roles)
-        if target.id in [y.id for y in message.author.roles]:
-            fn = 'lists/mentors.txt'
-            target = message.mentions[0]
-            a = open(fn)
-            output = []
-            temp = 0
-            for line in a:
-                if f'{target.id}' not in line:
-                    output.append(line)
-                else:
-                    temp += 1
-            a.close()
-            a = open(fn, 'w')
-            a.writelines(output)
-            a.close()
-            mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
-            await target.remove_roles(mentor_role)
-            if not temp == 0:
-                response = discord.Embed(title=f"✅ {target.display_name} is no longer a Mentor", color=0x77b255)
+    if message.guild:
+        if args:
+            if message.mentions:
+                try:
+                    target = discord.utils.find(lambda x: x.name.lower() == 'head mentor', message.guild.roles)
+                    if target.id in [y.id for y in message.author.roles]:
+                        fn = 'lists/mentors.txt'
+                        target = message.mentions[0]
+                        a = open(fn)
+                        output = []
+                        temp = 0
+                        for line in a:
+                            if f'{target.id}' not in line:
+                                output.append(line)
+                            else:
+                                temp += 1
+                        a.close()
+                        a = open(fn, 'w')
+                        a.writelines(output)
+                        a.close()
+                        mentor_role = discord.utils.get(message.guild.roles, name='Mentor')
+                        await target.remove_roles(mentor_role)
+                        if not temp == 0:
+                            response = discord.Embed(title=f"✅ {target.display_name} is no longer a Mentor", color=0x77b255)
+                        else:
+                            response = discord.Embed(title="❗ That user wasn't a Mentor", color=0xBE1931)
+                    else:
+                        response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
+                except AttributeError:
+                    response = discord.Embed(title="❗ I couldn't find the role 'Head Mentor'", color=0xBE1931)
             else:
-                response = discord.Embed(title="❗ That user wasn't a Mentor", color=0xBE1931)
+                response = discord.Embed(title='❗ A user mention required', color=0xBE1931)
         else:
-            response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
-    except AttributeError:
-        response = discord.Embed(title="❗ I couldn't find the role 'Head Mentor'", color=0xBE1931)
+            response = discord.Embed(title="❗ No input", color=0xBE1931)
+    else:
+        response = discord.Embed(title='🔒 You can\'t use that command in a DM', color=0xFFCC4d)
     await message.channel.send(embed=response)
