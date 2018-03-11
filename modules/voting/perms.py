38cea06eddffa2a613a3149707843ed7a6861254
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-11 16:35:11 -0400

    Changed how data gets parsed

--- modules/voting/perms.py
+++ modules/voting/perms.py
@@ -1,57 +1,50 @@
 import discord
+import json
 from checks import server_check
 
 
 async def ex(args, message, bot, invoke):
-    if server_check(message):
-        if message.author.permissions_in(message.channel).administrator:
-            with open('permissions/channels.txt', 'r') as file:
-                a = file.read()
-            with open('permissions/roles.txt', 'r') as file:
-                b = file.read()
-            with open('permissions/users.txt', 'r') as file:
-                c = file.read()
-            with open('permissions/warlords.txt', 'r') as file:
-                d = file.read()
-                if "warlords:" in d.lower():
-                    warlords = True
+    if message.guild:
+        if server_check(message):
+            if message.author.permissions_in(message.channel).administrator:
+                with open('permissions/channels.txt', 'r') as file:
+                    a = file.read()
+                    if '' == a:
+                        chn = 'None\n'
+                    else:
+                        chn = f'\n{a}'
+                with open('permissions/roles.json', encoding='utf-8') as vote_data_file:
+                    vote_file = json.loads(vote_data_file.read())
+                rol_p = vote_file['roles']
+                roles = ''
+                for role in rol_p:
+                    roles += f'<@&{role}>\n'
+                if '' == roles:
+                    rol = 'None\n'
                 else:
-                    warlords = False
-                if warlords:
-                    dmw = "Warlords"
+                    rol = f'\n{roles}'
+                with open('permissions/users.txt', 'r') as file:
+                    c = file.read()
+                    if '' == c:
+                        usr = 'None\n'
+                    else:
+                        usr = f'\n{c}'
+                with open('permissions/roles.json') as roles_data:
+                    roles_file = json.loads(roles_data.read())
+                dm_p = roles_file['dm']
+                if 'Yes' in dm_p:
+                    dm = 'Yes\n'
                 else:
-                    dmw = ""
-            with open('permissions/generals.txt', 'r') as file:
-                d = file.read()
-                if "generals:" in d.lower():
-                    generals = True
-                else:
-                    generals = False
-                if generals:
-                    dmg = "Generals"
-                else:
-                    dmg = ""
-            with open('permissions/officers.txt', 'r') as file:
-                e = file.read()
-                if "officers:" in e.lower():
-                    officers = True
-                else:
-                    officers = False
-                if officers:
-                    dmo = "Officers"
-                else:
-                    dmo = ""
-            response = discord.Embed(title='🔏 **Poll permissions:**\n',
-                                     description='**Channels:**\n'
-                                     '%s'
-                                     '**Roles:**\n'
-                                     '%s'
-                                     '**Users:**\n'
-                                     '%s'
-                                     '**DMs:**\n'
-                                     '%s\n%s\n%s' % (a, b, c, dmw, dmg, dmo), color=0xFFCC4D)
+                    dm = 'No\n'
+                response = discord.Embed(title='🔏 **Poll permissions:**\n',
+                                         description=f'**DMs Allowed:** {dm}'
+                                         f'**Channels:** {chn}'
+                                         f'**Roles:** {rol}'
+                                         f'**Users:** {usr}', color=0xFFCC4d)
+            else:
+                response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
         else:
-            response = discord.Embed(title='⛔ Access denied: Administrator required', color=0xBE1931)
+            response = discord.Embed(title='🔒 You can\'t use that command on this server', color=0xFFCC4d)
     else:
-        response = discord.Embed(title='🔒 You can\'t use that command on this server', color=0xFFCC4d)
+        response = discord.Embed(title='🔒 You can\'t use that command in a DM', color=0xFFCC4d)
     await message.channel.send(embed=response)
