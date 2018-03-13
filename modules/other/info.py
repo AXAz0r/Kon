842939e000b6b2f88d0f31ec19c1bd082c7fe1b5
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-12 21:08:50 -0400

    Added ID line and updated command count

--- modules/other/info.py
+++ modules/other/info.py
@@ -7,8 +7,8 @@ async def ex(args, message, bot, invoke):
     response = discord.Embed(color=0xA5FFF6)
     response.set_author(name='Kon: Fox Bot', icon_url=avatar)
     response.add_field(name='Information',
-                       value=f'Created on: **Nov 13, 2017**\nSource code: [Github]({github_url})\n'
-                             f'Owner: **Shifty9#0995**\nCommands: **35**\nLatency: **{int(bot.latency * 1000)}ms**\n'
-                             f'ID: **{bot.user.id}**', inline=True)
+                       value=f'ID: **{bot.user.id}**\nCreated on: **Nov 13, 2017**\nSource code: [Github]({github_url})'
+                             f'\nOwner: **Shifty9#0995**\nCommands: **36**\nLatency: **{int(bot.latency * 1000)}ms**',
+                       inline=True)
     response.set_footer(text='Credit to AXAz0r#0001 for help')
     await message.channel.send(embed=response)
