e7b79d996c8c649c5611a5e723f4720fbee81e0b
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-22 18:31:08 -0400

    Created command

---
+++ modules/banking/sell.py
@@ -0,0 +1,49 @@
+import discord
+import json
+
+
+def dump_list(number, market_data, item, price, qty):
+    market_list = market_data.get('market')
+    num = 1
+    for listing in market_list:
+        num += 1
+    data = {"number": number, "item": item, "price": price, "quantity": qty}
+    market_list.append(data)
+    market_data.update({'market': market_list})
+    with open('lists/market.json', 'w', encoding='utf-8') as market_file:
+        json.dump(market_data, market_file, indent=1)
+
+
+async def ex(args, message, bot, invoke):
+    role = discord.utils.find(lambda x: x.id == xxxxxxxxxxxxxxxxxx, message.guild.roles)
+    if role:
+        if args:
+            all_qry = ' '.join(args)
+            item = ''.join(all_qry.split('; ')[0]).title()
+            price = ''.join(all_qry.split('; ')[1])
+            qty = ''.join(all_qry.split('; ')[2])
+            if 'p' in price.lower():
+                price = price
+            else:
+                price = price + 'p'
+            if 'x' in qty.lower():
+                qty = qty
+            else:
+                qty = qty + 'x'
+            if item and price and qty:
+                with open('lists/market.json', encoding='utf-8') as file:
+                    market_data = json.load(file)
+                    i = 1
+                    for x in market_data['market']:
+                        i += 1
+                    number = f'{i}'
+                dump_list(number, market_data, item, price, qty)
+                response = discord.Embed(title=f'✅ Your item has been listed', color=0x77B255)
+            else:
+                response = discord.Embed(title='❗ Invalid arguments', color=0xBE1931)
+                response.set_footer(text='^sell item; 10p; 2x')
+        else:
+            response = discord.Embed(title='❗ No input', color=0xBE1931)
+    else:
+        response = discord.Embed(title="⛔ Access denied: Banker required", color=0xBE1931)
+    await message.channel.send(embed=response)
