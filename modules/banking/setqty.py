5b8a1108e5ae7f3e7fa99185a09bc1c84ee1c51f
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-22 22:56:30 -0400

    Removed ID

--- modules/banking/setqty.py
+++ modules/banking/setqty.py
@@ -10,7 +10,7 @@ def update_qty(market_data, number: int, qty):
 
 
 async def ex(args, message, bot, invoke):
-    role = discord.utils.find(lambda x: x.id == 404736328387919882, message.guild.roles)
+    role = discord.utils.find(lambda x: x.id == xxxxxxxxxxxxxxxxxx, message.guild.roles)
     if role:
         if role.id in [y.id for y in message.author.roles]:
             if args:
