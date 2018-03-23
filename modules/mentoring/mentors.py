064f446e6fb4f99732f9f25cd4c80f5a44121887
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-23 15:55:20 -0400

    Rewrote command (again)

--- modules/mentoring/mentors.py
+++ modules/mentoring/mentors.py
@@ -2,15 +2,7 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    role_search = discord.utils.find(lambda x: x.id == xxxxxxxxxxxxxxxxxx, message.guild.roles)
-    if role_search:
-        mentors = ''
-        for member in message.guild.members:
-            member_role_search = discord.utils.find(lambda x: x.id == role_search.id, member.roles)
-            if member_role_search:
-                mentors += f'{member.mention}\n'
-        response = discord.Embed(color=role_search.color)
-        response.add_field(name='🎓 Mentors', value=f'{mentors}')
-    else:
-        response = discord.Embed(title=f'🔍 I couldn\'t find the Mentor role', color=0x696969)
+    with open('lists/mentors.txt', 'r') as file:
+        mentors = file.read()
+    response = discord.Embed(title='🎓 Mentors', description=f'{mentors}', color=0xFF0000)
     await message.channel.send(embed=response)
