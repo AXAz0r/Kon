a3da75b004724a3b66ca931a1693ce307603e614
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-22 22:50:44 -0400

    Changed order of checks

--- modules/mentoring/addmentor.py
+++ modules/mentoring/addmentor.py
@@ -3,11 +3,11 @@ import discord
 
 async def ex(args, message, bot, invoke):
     if message.guild:
-        if args:
-            if message.mentions:
-                role = discord.utils.find(lambda x: x.id == 376195917407191040, message.guild.roles)
-                if role:
-                    if role.id in [y.id for y in message.author.roles]:
+        role = discord.utils.find(lambda x: x.id == 376195917407191040, message.guild.roles)
+        if role:
+            if role.id in [y.id for y in message.author.roles]:
+                if args:
+                    if message.mentions:
                         if len(args) >= 3:
                             user = args[0]
                             zone = args[1]
@@ -21,15 +21,14 @@ async def ex(args, message, bot, invoke):
                             response = discord.Embed(title=f"✅ {target.display_name} is now a Mentor", color=0x77B255)
                         else:
                             response = discord.Embed(title='❗ Not enough inputs', color=0xBE1931)
-                            response.set_footer(text='@Username UTC #')
                     else:
-                        response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
+                        response = discord.Embed(title='❗ A user mention required', color=0xBE1931)
                 else:
-                    response = discord.Embed(title="❗ I couldn't find the role 'Head Mentor'", color=0xBE1931)
+                    response = discord.Embed(title="❗ No input", color=0xBE1931)
             else:
-                response = discord.Embed(title='❗ A user mention required', color=0xBE1931)
+                response = discord.Embed(title="⛔ Access denied: Head Mentor required", color=0xBE1931)
         else:
-            response = discord.Embed(title="❗ No input", color=0xBE1931)
+            response = discord.Embed(title=f'🔍 I couldn\'t find the Head Mentor role', color=0x696969)
     else:
         response = discord.Embed(title='🔒 You can\'t use that command in a DM', color=0xFFCC4d)
     await message.channel.send(embed=response)
