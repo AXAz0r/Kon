537fd29c1b984af6a58d48b2334006d15a2e276d
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-22 22:49:19 -0400

    Changed order of checks

--- modules/banking/setqty.py
+++ modules/banking/setqty.py
@@ -11,29 +11,32 @@ def update_qty(market_data, number: int, qty):
 
 async def ex(args, message, bot, invoke):
     role = discord.utils.find(lambda x: x.id == 404736328387919882, message.guild.roles)
-    if args:
-        if role:
-            number, quantity = args[0].split(':')
-            if str.isdigit(number) and str.isdigit(quantity):
-                number = int(number)
-                if number and quantity:
-                    with open('lists/market.json', encoding='utf-8') as file:
-                        market_data = json.load(file)
-                    market_list = market_data.get('market')
-                    x = 0
-                    for market_item in market_list:
-                        x += 1
-                    if number <= x:
-                        update_qty(market_data, number, quantity)
-                        response = discord.Embed(title=f'✅ Quantity updated', color=0x77B255)
+    if role:
+        if role.id in [y.id for y in message.author.roles]:
+            if args:
+                number, quantity = args[0].split(':')
+                if str.isdigit(number) and str.isdigit(quantity):
+                    number = int(number)
+                    if number and quantity:
+                        with open('lists/market.json', encoding='utf-8') as file:
+                            market_data = json.load(file)
+                        market_list = market_data.get('market')
+                        x = 0
+                        for market_item in market_list:
+                            x += 1
+                        if number <= x:
+                            update_qty(market_data, number, quantity)
+                            response = discord.Embed(title=f'✅ Quantity updated', color=0x77B255)
+                        else:
+                            response = discord.Embed(title=f'🔍 I couldn\'t find that listing', color=0x696969)
                     else:
-                        response = discord.Embed(title=f'🔍 I couldn\'t find that listing', color=0x696969)
+                        response = discord.Embed(title='❗ Invalid arguments', color=0xBE1931)
                 else:
-                    response = discord.Embed(title='❗ Invalid arguments', color=0xBE1931)
+                    response = discord.Embed(title='❗ Input must be numbers', color=0xBE1931)
             else:
-                response = discord.Embed(title='❗ Input must be numbers', color=0xBE1931)
+                response = discord.Embed(title='❗ No input', color=0xBE1931)
         else:
             response = discord.Embed(title="⛔ Access denied: Banker required", color=0xBE1931)
     else:
-        response = discord.Embed(title='❗ No input', color=0xBE1931)
+        response = discord.Embed(title=f'🔍 I couldn\'t find the Head Mentor role', color=0x696969)
     await message.channel.send(embed=response)
