2736f69b79979565004faea157ef866370506183
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-22 22:49:36 -0400

    Corrected role check

--- modules/banking/sell.py
+++ modules/banking/sell.py
@@ -15,35 +15,37 @@ def dump_list(number, market_data, item, price, qty):
 
 
 async def ex(args, message, bot, invoke):
-    role = discord.utils.find(lambda x: x.id == xxxxxxxxxxxxxxxxxx, message.guild.roles)
+    role = discord.utils.find(lambda x: x.id == 404736328387919882, message.guild.roles)
     if role:
-        if args:
-            all_qry = ' '.join(args)
-            item = ''.join(all_qry.split('; ')[0]).title()
-            price = ''.join(all_qry.split('; ')[1])
-            qty = ''.join(all_qry.split('; ')[2])
-            if 'p' in price.lower():
-                price = price
+        if role.id in [y.id for y in message.author.roles]:
+            if args:
+                all_qry = ' '.join(args)
+                item = ''.join(all_qry.split('; ')[0]).title()
+                price = ''.join(all_qry.split('; ')[1])
+                qty = ''.join(all_qry.split('; ')[2])
+                if 'p' in price.lower():
+                    price = price
+                else:
+                    price = price + 'p'
+                if 'x' in qty.lower():
+                    qty = qty
+                else:
+                    qty = qty + 'x'
+                if item and price and qty:
+                    with open('lists/market.json', encoding='utf-8') as file:
+                        market_data = json.load(file)
+                        i = 1
+                        for x in market_data['market']:
+                            i += 1
+                        number = f'{i}'
+                    dump_list(number, market_data, item, price, qty)
+                    response = discord.Embed(title=f'✅ Listing added', color=0x77B255)
+                else:
+                    response = discord.Embed(title='❗ Invalid arguments', color=0xBE1931)
             else:
-                price = price + 'p'
-            if 'x' in qty.lower():
-                qty = qty
-            else:
-                qty = qty + 'x'
-            if item and price and qty:
-                with open('lists/market.json', encoding='utf-8') as file:
-                    market_data = json.load(file)
-                    i = 1
-                    for x in market_data['market']:
-                        i += 1
-                    number = f'{i}'
-                dump_list(number, market_data, item, price, qty)
-                response = discord.Embed(title=f'✅ Your item has been listed', color=0x77B255)
-            else:
-                response = discord.Embed(title='❗ Invalid arguments', color=0xBE1931)
-                response.set_footer(text='^sell item; 10p; 2x')
+                response = discord.Embed(title='❗ No input', color=0xBE1931)
         else:
-            response = discord.Embed(title='❗ No input', color=0xBE1931)
+            response = discord.Embed(title="⛔ Access denied: Banker required", color=0xBE1931)
     else:
-        response = discord.Embed(title="⛔ Access denied: Banker required", color=0xBE1931)
+        response = discord.Embed(title=f'🔍 I couldn\'t find the Banker role', color=0x696969)
     await message.channel.send(embed=response)
