eb4017396975f6a2315977f3eb98ba8292de6e60
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-22 22:56:17 -0400

    Removed ID

--- modules/banking/sell.py
+++ modules/banking/sell.py
@@ -15,7 +15,7 @@ def dump_list(number, market_data, item, price, qty):
 
 
 async def ex(args, message, bot, invoke):
-    role = discord.utils.find(lambda x: x.id == 404736328387919882, message.guild.roles)
+    role = discord.utils.find(lambda x: x.id == xxxxxxxxxxxxxxxxxx, message.guild.roles)
     if role:
         if role.id in [y.id for y in message.author.roles]:
             if args:
