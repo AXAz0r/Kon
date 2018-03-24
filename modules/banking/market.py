bceb0d9c3ecb7ce4a7ff75a0cd5ee6258c763f8c
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-03-24 02:27:28 -0400

    Added alphabetizer

--- modules/banking/market.py
+++ modules/banking/market.py
@@ -3,17 +3,45 @@ import json
 from humanfriendly.tables import format_pretty_table as boop
 
 
-def parser():
+def sorter():
     with open('lists/market.json') as file:
         market_data = json.load(file)
+    market_list = market_data.get('market')
     data = market_data['market']
-    i = 0
+    a = 0
     to_list = []
     for item in data:
-        to_data = list(data[i].values())
+        to_data = list(data[a].values())
         to_list.append(to_data)
-        i += 1
-    return to_list
+        a += 1
+    b = 1
+    to_sorted = sorted(to_list, key=lambda x: x[1])
+    sorted_list = []
+    for market_item in to_sorted:
+        market_item[0] = f'{b}'
+        sorted_list.append(market_item)
+        b += 1
+    c = 0
+    for market_item in market_list:
+        if market_item['number']:
+            market_item['number'] = f'{sorted_list[c][0]}'
+        if market_item['item']:
+            market_item['item'] = f'{sorted_list[c][1]}'
+        if market_item['price']:
+            market_item['price'] = f'{sorted_list[c][2]}'
+        if market_item['quantity']:
+            market_item['quantity'] = f'{sorted_list[c][3]}'
+        c += 1
+    d = 0
+    output_list = []
+    for item in data:
+        to_data = list(data[d].values())
+        output_list.append(to_data)
+        d += 1
+    market_data.update({'market': market_list})
+    with open('lists/market.json', 'w', encoding='utf-8') as market_file:
+        json.dump(market_data, market_file, indent=1)
+    return output_list
 
 
 async def ex(args, message, bot, invoke):
@@ -30,7 +58,7 @@ async def ex(args, message, bot, invoke):
     start_range = (page_number - 1) * 10
     end_range = page_number * 10
     headers = ['Num', 'Item', 'Price', 'Qty']
-    item_list = parser()
+    item_list = sorter()
     to_sorted = sorted(item_list, key=lambda x: int(x[0]))
     inv = to_sorted[start_range:end_range]
     if inv:
