88c0442eb53919b26e62e1dcfa686505ce695bd7
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-04-09 14:15:33 -0400

    Added new command commands_list and imported them

--- koncore.py
+++ koncore.py
@@ -8,7 +8,7 @@ from modules.banking import additem, bankers, buyitem, delitem, delseller, marke
 from modules.information import members, help, info, commands_help, ping, rolepop, user
 from modules.mentoring import addmentor, apply, delline, delmentor, mentors, requests
 from modules.owner import eval, reboot, setavatar, setstatus, setusername
-from modules.utility import color, dance, kon, link, match, mute, purge, random, roll, send, sleep, unmute
+from modules.utility import color, dance, genban, genunban, kon, link, match, mute, purge, random, roll, send, sleep, unmute
 from modules.voting import clrperms, clrvotes, password, permit, perms, setpassword, unpermit, vote, voters, votes
 from discord.ext import commands
 
@@ -52,6 +52,8 @@ commands_list = {
     "delmentor": delmentor,
     "delseller": delseller,
     "eval": eval,
+    "genban": genban,
+    "genunban": genunban,
     "help": help,
     "info": info,
     "kon": kon,
