19a42ddf7cb6c212b9d5d590df1338f0256aeec5
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-04-19 16:51:07 -0400

    Added mention to commands_list and imported it

--- koncore.py
+++ koncore.py
@@ -8,7 +8,7 @@ from modules.banking import additem, bankers, buyitem, delitem, delseller, marke
 from modules.information import members, help, info, commands_help, ping, rolepop, user
 from modules.mentoring import addmentor, apply, delline, delmentor, mentors, requests
 from modules.owner import eval, reboot, setavatar, setstatus, setusername
-from modules.utility import color, dance, genban, genunban, id, kon, link, match, mute, purge, random, roll, send, sleep, unmute
+from modules.utility import color, dance, genban, genunban, id, kon, link, match, mention, mute, purge, random, roll, send, sleep, unmute
 from modules.voting import clrperms, clrvotes, password, permit, perms, setpassword, unpermit, vote, voters, votes
 from discord.ext import commands
 
@@ -62,6 +62,7 @@ commands_list = {
     "match": match,
     "market": market,
     "members": members,
+    "mention": mention,
     "mentors": mentors,
     "mute": mute,
     "password": password,
