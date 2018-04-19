0e431a7c851572d952aa656c7bf2f7c259fc5252
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-04-19 13:07:22 -0400

    Changed embed color

--- modules/information/ping.py
+++ modules/information/ping.py
@@ -2,5 +2,5 @@ import discord
 
 
 async def ex(args, message, bot, invoke):
-    response = discord.Embed(description=f"`🏓 {int(bot.latency * 1000)}ms`", color=0xA5FFF6)
+    response = discord.Embed(description=f"`🏓 {int(bot.latency * 1000)}ms`", color=0xAA2C15)
     await message.channel.send(embed=response)
