f78e16f3f5d7070c296b452bfb94b87ce5792c17
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-04-20 15:23:02 -0400

    Updated command to use a live command count

--- modules/information/info.py
+++ modules/information/info.py
@@ -1,14 +1,15 @@
 import discord
+from koncore import commands_list
 
 
 async def ex(args, message, bot, invoke):
     avatar = 'https://i.imgur.com/qAwBsHO.png'
     github_url = 'https://github.com/Shifty6/Kon'
+    cmd_count = len(commands_list)
+    info_block = f'ID: **{bot.user.py.id}**\nCreated on: **Nov 13, 2017**\nSource code: [Github]({github_url})'
+    info_block += f'\nOwner: **Shifty9#0995**\nCommands: **{cmd_count}**\nLatency: **{int(bot.latency * 1000)}ms**'
     response = discord.Embed(color=0xA5FFF6)
     response.set_author(name='Kon: Fox Bot', icon_url=avatar)
-    response.add_field(name='Information',
-                       value=f'ID: **{bot.user.id}**\nCreated on: **Nov 13, 2017**\nSource code: [Github]({github_url})'
-                             f'\nOwner: **Shifty9#0995**\nCommands: **36**\nLatency: **{int(bot.latency * 1000)}ms**',
-                       inline=True)
-    response.set_footer(text='Credit to AXAz0r#0001 for help')
+    response.add_field(name='Information', value=info_block, inline=True)
+    response.set_footer(text='Credit to AXAz0r#0001 for help')  # <3 love ya shif
     await message.channel.send(embed=response)
