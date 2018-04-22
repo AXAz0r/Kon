28ebc9fe80d934a5f58a324c9eed11ded2f27eb8
Shifty6 <33166535+Shifty6@users.noreply.github.com>
2018-04-22 17:18:00 -0400

    Changed error response

--- modules/owner/eval.py
+++ modules/owner/eval.py
@@ -18,6 +18,6 @@ async def ex(args: list, message: discord.Message, bot, invoke):
                 status = discord.Embed(title='✅ Executed', color=0x77B255)
                 status.add_field(name='Results', value=f'\n```\n{output}\n```')
             except Exception as e:
-                status = discord.Embed(color=0xBE1931, title='❗ Error')
-                status.add_field(name='Execution Failed', value=f'{e}')
+                status = discord.Embed(color=0xBE1931, description=f'{e}')
+                status.set_author(name='Error', icon_url='https://i.imgur.com/S7aUuLU.png')
         await message.channel.send(None, embed=status)
